1589005eec5d1cfc28ec623df584d24cc3b4b5bc
softformer <softformer@gmail.com>
2013-04-18 22:07:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'd55673dc5b19c90866e795d8b412ee5105185aa19b16f1c63058e42328d12cf6ff9c630605a4f421c7be5a111730ef4c2ae35b0b6e6075e5748e894053109d06'
